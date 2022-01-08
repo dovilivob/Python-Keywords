eacaa27890ac90950ca0bd13db2e3f70925919d5
dovilivob <david.iii.chiu@gmail.com>
2022-01-08 16:51:35 +0800

    bin()

---
+++ Function/Built-in-Function/@bin.py
@@ -0,0 +1,9 @@
+# bin(int)
+# return the binary version of a specified integer
+# The result always start with the prefix 0b
+
+print(bin(36))
+# output: 0b100100
+
+print(bin(-77))
+# output: -0b1001101
