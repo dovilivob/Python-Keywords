9fa366b3963551361d1a9decc84d740e278b31de
dovilivob <david.iii.chiu@gmail.com>
2022-01-08 16:54:19 +0800

    bool()

---
+++ Function/Built-in-Function/@bool.py
@@ -0,0 +1,12 @@
+# bool(object)
+# Always return True, unless:
+#   0, False, None, [], {}, ()
+
+print(bool(3737))
+# output: True
+
+print(bool([[]]))
+# output: True
+
+print(bool([]))
+# output: False
