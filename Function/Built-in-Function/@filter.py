9d67a8c0e9443c6a559f310fe9c7f9fb650e2cb7
dovilivob <david.iii.chiu@gmail.com>
2022-01-09 23:38:32 +0800

    filter()

---
+++ Function/Built-in-Function/@filter.py
@@ -0,0 +1,23 @@
+# return an iterator were the items are filtered through a function to test if the item is accepted or not.
+
+# filter(function, iterable)
+
+ages = [4, 23, 22, 55, 10, 32, 5, 92]
+
+def func(x):
+    if x < 18:
+        return 0
+    else:
+        return 1
+
+adults = filter(func, ages)
+
+for x in adults:
+    print(x)
+    
+# output: 
+# 23
+# 22
+# 55
+# 32
+# 92
