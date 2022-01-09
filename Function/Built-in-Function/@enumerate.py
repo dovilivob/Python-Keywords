245dee5d8a40d61781451bc20177b8e8b6ea08dd
dovilivob <david.iii.chiu@gmail.com>
2022-01-09 23:25:10 +0800

    enumerate()

---
+++ Function/Built-in-Function/@enumerate.py
@@ -0,0 +1,19 @@
+# Takes a collection and returns it as an enumerate object.
+# Adds a counter as the key of the enumerate object.
+
+
+# enumerate(iterable, start)
+# start: A Number. Defining the start number of the enumerate object. Default 0.
+
+x = ('apple', 'banana', 'cherry')
+print(enumerate(x))
+
+x = ('hello', 9)
+print(enumerate(x))
+
+y = x
+print(enumerate(y))
+# output: 
+#   <enumerate object at 0x7fa448522a40>
+#   <enumerate object at 0x7fa448522a40 >
+#   <enumerate object at 0x7fa448522a40 >
