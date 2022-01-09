7c6ab825fda5faefc2934faff14f9d3e4f4f0f0f
dovilivob <david.iii.chiu@gmail.com>
2022-01-09 21:54:18 +0800

    complex

---
+++ Function/Built-in-Function/@complex.py
@@ -0,0 +1,13 @@
+# complex(real, imaginary)
+
+
+x = complex(3, 6)
+
+print(x, abs(x))
+# output: 
+#   (3+6j) 6.708203932499369
+
+y = complex('5+9j')
+print(y, abs(y))
+# output:
+#   (5+9j) 10.295630140987
