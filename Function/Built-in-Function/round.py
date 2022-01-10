005cd2a6a6b1a934bb6391e57ef48f2eb29f6952
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 19:21:05 +0800

    round()

---
+++ Function/Built-in-Function/round.py
@@ -0,0 +1,16 @@
+# return a float that is rounded version of specified number,
+# with the specified number of decimal
+# The default number of decimal is 0,
+# meaning that the function will return the nearest integer.
+
+# round(number, digits)
+
+x = round(3.347389, 2)
+print(x)
+# output: 3.35
+
+print(round(4.349348))
+# output: 4
+
+print(round(4.5100))
+# output: 5
