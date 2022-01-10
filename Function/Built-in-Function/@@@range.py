3c5bef7d1bce9f3f7dafed1811d562b8d8440ca6
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 19:14:35 +0800

    range()

---
+++ Function/Built-in-Function/@@@range.py
@@ -0,0 +1,28 @@
+# return a sequence of numbers,
+# starting from 0 by default,
+# and increments by 1 by default,
+# and stops before a specified number.
+
+# range(start, stop, step)
+# step: An integer specifying the incrementation.
+# Default 1
+
+x = range(3)
+
+for n in x:
+    print(n)
+# output:
+# 0
+# 1
+# 3
+
+for i in range(8, 90, 12):
+    print(i)
+# output:
+# 8
+# 20
+# 32
+# 44
+# 56
+# 68
+# 80
