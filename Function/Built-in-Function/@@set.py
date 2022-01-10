051dc539cd8216dc172eeda724738c9b6f252fc6
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 19:23:45 +0800

    set()

---
+++ Function/Built-in-Function/@@set.py
@@ -0,0 +1,10 @@
+# Creates a set object
+# The items in a set list are unordered,
+# so it will appear in random order.
+
+# set(iterable)
+
+x = set(('asshole', 'bitch', 'cunt', 'dumb', 'eat shit', 'fuck'))
+print(x)
+# output:
+#   {'fuck', 'bitch', 'dumb', 'eat shit', 'asshole', 'cunt'}
