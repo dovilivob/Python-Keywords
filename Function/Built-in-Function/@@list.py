1c2dbc76e302cb116896b407ab72f2a4c002ec68
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 15:35:32 +0800

    list()

---
+++ Function/Built-in-Function/@@list.py
@@ -0,0 +1,12 @@
+# Creates a list object
+# list(iterable)
+
+x = list(('a', 'b', 'c'))
+print(x)
+# output:
+#   ['a', 'b', 'c']
+
+x = list(dict(a=3, b=2, c=1))
+print(x)
+# output:
+#   ['a', 'b', 'c']
