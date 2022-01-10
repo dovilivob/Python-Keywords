935419741d07096bbcf28edac9f470f3d3be21e4
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 14:54:20 +0800

    globals()

---
+++ Function/Built-in-Function/@globals.py
@@ -0,0 +1,11 @@
+# return the global symbol table as a dictionary
+# A symbol table contains necessary information about the current program
+
+x = globals()
+print(x)
+# output:
+#   {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x7f9bc212cc10>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/dovilivob/Programming/Python-Keywords/Function/Built-in-Function/globals.py', '__cached__': None, 'x': {...}}
+
+print(x['__file__'])
+# output:
+#   /home/dovilivob/Programming/Python-Keywords/Function/Built-in-Function/globals.py
