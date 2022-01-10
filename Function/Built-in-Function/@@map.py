f30f5fb528c2c9625a0b5513d23994ae5d6cad00
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 15:51:51 +0800

    map()

---
+++ Function/Built-in-Function/@@map.py
@@ -0,0 +1,19 @@
+# map(function, iterable)
+# Executes a specified function for each item in an iterable.
+# the item is sent to the function as a parameter
+
+
+def myfunc(n):
+    return len(n)
+
+
+x = map(myfunc, ('apple', 'banana', 'Canada'))
+print(list(x))
+# output: [5, 6, 6]
+
+def plus(a, b):
+    return a + b
+
+x = map(plus, (1, 3, 4), (3, 5, 7))
+print(list(x))
+# output: [4, 8, 11]
