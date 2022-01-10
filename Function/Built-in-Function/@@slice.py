563510fc0fe62b112a2b9d083870f2b2efd50dcf
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 19:30:50 +0800

    slice()

---
+++ Function/Built-in-Function/@@slice.py
@@ -0,0 +1,15 @@
+# return to specify how to slice a sequence.
+# You can specify where to start the slicing,
+# and where to end. You can also specify the step,
+# which allows you to e.g. slice only every other item.
+
+start = 0
+end = 2
+step = 1
+x = slice(start, end, step)
+
+a = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i')
+x = slice(0, 6, 2)
+print(a[x])
+# output:
+#   ('a', 'c', 'e')
