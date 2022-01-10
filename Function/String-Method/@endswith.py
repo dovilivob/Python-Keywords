105fe8ac1a8b569e82c54e35f866db18b97d9fd6
dovilivob <david.iii.chiu@gmail.com>
2022-01-10 21:27:10 +0800

    endswitch()

---
+++ Function/String-Method/@endswith.py
@@ -0,0 +1,12 @@
+# Returns True if the string ends with the specified value, otherwise False
+
+string = "Hello, welcome to my world!"
+
+(value, start, end) = ('.', 0, len(string))
+
+print(string.endswith(value, start, end))
+# Output: False
+
+value = 'my world!'
+print(string.endswith(value, start, end))
+# Output: True
